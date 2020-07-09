1c235d07473f23fee1a5ed0d31c259e89623bff5
Alexandre Marques <alexandreacm.marques@gmail.com>
2020-07-08 23:58:10 -0300

    remoção de arquivos

--- app/src/main/java/com/arrobatecinformatica/agendacontatosrealm/constants/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.arrobatecinformatica.agendacontatosrealm.constants;
-
-public class Constants {
-    public static  final String TAG = "Contato";
-    public static final String NOMEBANCO = "Agenda";
-    public static final int VERSAOBANCO = 1;
-
-}
--- app/src/main/java/com/arrobatecinformatica/agendacontatosrealm/interfaces/IRecycleView.java
+++
@@ -1,7 +0,0 @@
-package com.arrobatecinformatica.agendacontatosrealm.interfaces;
-
-import com.arrobatecinformatica.agendacontatosrealm.model.Contato;
-
-public interface IRecycleView {
-     void IRecycleView_OnItemClickListener(Contato modelContato);
-}
